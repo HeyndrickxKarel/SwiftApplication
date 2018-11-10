b325f4e4b1209e1c20981fb3a285539b96c446a0
Karel Heyndrickx <karel.heyndrickx.y7711@student.hogent.be>
2018-11-10 17:54:48 +0100

    created project

---
+++ RijQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RijQuiz
+//
+//  Created by Karel Heyndrickx on 10/11/2018.
+//  Copyright © 2018 Karel Heyndrickx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
